66f6b5b7353b3fddd76ca243e8b70db14b27e711
gongym <1967988842@qq.com>
2024-08-15 09:18:48 +0800

    update main

--- main/debug.mjs
+++ main/debug.mjs
@@ -1,5 +1,19 @@
 #!/usr/bin/env zx
 
-$`rollup -c`
+import { fs } from 'zx'
+
+console.log()
+console.log('compiling...')
+if (!(await fs.exists('target'))) {
+  await fs.mkdir('target')
+} else {
+  if (await fs.exists('target/index.js')) {
+    await fs.rm('target/index.js')
+  }
+}
+await $`rollup -c`
+
+console.log()
+console.log('debugging...')
 cd('target')
 $`electron .`
