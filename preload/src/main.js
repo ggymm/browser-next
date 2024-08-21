60d2cbca4da26721a0c09a53552ca8a59241aebb
gongym <1967988842@qq.com>
2024-08-21 11:27:11 +0800

    update preload

--- preload/src/main.js
+++ preload/src/main.js
@@ -1,10 +1,11 @@
-import { contextBridge, ipcRenderer } from 'electron'
+import { contextBridge, ipcRenderer } from "electron";
 
-contextBridge.exposeInMainWorld('electron', {
-    ipcRenderer: {
-        ...ipcRenderer,
-        on: ipcRenderer.on.bind(ipcRenderer),
-        once: ipcRenderer.once.bind(ipcRenderer),
-        removeAllListeners: ipcRenderer.removeAllListeners.bind(ipcRenderer)
-    }
-})
+contextBridge.exposeInMainWorld("electron", {
+  ipcRenderer: {
+    on: ipcRenderer.on.bind(ipcRenderer),
+    once: ipcRenderer.once.bind(ipcRenderer),
+    send: ipcRenderer.send.bind(ipcRenderer),
+    sendSync: ipcRenderer.sendSync.bind(ipcRenderer),
+    removeAllListeners: ipcRenderer.removeAllListeners.bind(ipcRenderer),
+  },
+});
